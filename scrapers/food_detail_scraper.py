7ef9cdfe499dd82cb7c1d568269974c78c815859
jaspreetsinghsodhi <jaspreetsodhi02@gmail.com>
2023-12-05 00:56:15 +0530

    task-1 + error handling

--- scrapers/food_detail_scraper.py
+++ scrapers/food_detail_scraper.py
@@ -1,127 +1,4 @@
 
-# # Neccessary imports added ---
-# import csv
-# import selenium
-# from selenium import webdriver
-# import requests
-# import pandas as pd
-# import csv
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.chrome.options import Options
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-# from selenium.webdriver.chrome.service import Service
-
-
-
-# #Path to chromedriver from localmachine - 
-# chromedriver_path = "/Users/jaspreetSinghSodhi/downloads/chromedriver"
-
-
-# chrome_options = Options()
-# chrome_options.add_argument("--disable-blink-features=AutomationControlled")
-# chrome_options.add_argument("--start-maximized") 
-
-# # starting browser & settings --
-# driver = webdriver.Chrome( options=chrome_options)
-
-
-
-
-
-# def get_food_data(curfoodtype):
-    
-
-#     try:
-#         driver.get('https://magicpin.in/New-Delhi/Paharganj/Restaurant/Eatfit/store/61a193/delivery/')
-
-
-#         # added a explicit wait for the page to load completely
-#         wait = WebDriverWait(driver, timeout=10)
-
-
-#         food_names = []
-#         food_prices = []
-
-
-#         # getting the food prices
-#         avaliable_meal_prices = wait.until(EC.presence_of_all_elements_located((By.XPATH, f"//article[@id ='{curfoodtype}']//div//section//div//article//p//span[@class='itemPrice']")))
-
-#         # getting the food names
-#         avaliable_meal_names  = wait.until(EC.presence_of_all_elements_located((By.XPATH, f"//article[@id ='{curfoodtype}']//div//section//div//article//p[@class='itemName']")))
-       
-#         # print the food names
-#         if avaliable_meal_names is not None:
-
-#             for curfoodname in avaliable_meal_names:
-#                 food_names.append(curfoodname.text)
-        
-#         else:
-
-#             print(f'No food names found for {curfoodtype}')
-
-        
-#         # print the food prices
-
-#         if avaliable_meal_prices is not None:
-
-#             for curfoodprice in avaliable_meal_prices:
-#                 food_prices.append(curfoodprice.text)
-        
-#         else:
-
-#             print(f'No food prices found for {curfoodtype}')
-
-
-    
-
-#     except Exception as e:
-#         print(f'{e} - error occured while opening the website')
-#         driver.quit()
-        
-#     return food_names, food_prices
-
-
-# def data_processing():
-#     pass
-
-
-# def make_csv( food_names , food_prices):
-
-#     df = pd.DataFrame(list(zip(food_names, food_prices)), columns =['Food Name', 'Food Price'])
-#     df.to_csv('food_data.csv', index=False)
-
-    
-
-
-
-
-# if __name__ == "__main__":
-
-#     food_types = ['Kulcha Burger' , 'Chinese Fried Rice & Noodle Bowl' , '3 Layer Rice Bowl' , 'Fruit Pop' , 'Indian Thalis' , 'Gourmet By EatFit' , 'Breakfast And Snacks' , 'Healthy Khichdi' , 'A La Carte' , 'Desserts & Juices']
-
-#     total_food_names = []
-#     total_food_prices = []
-
-#     for curfoodtype in food_types:
-#        food_names, food_prices =  get_food_data(curfoodtype)
-#        total_food_names.extend(food_names)
-#        total_food_prices.extend(food_prices)
-    
-#     make_csv(total_food_names, total_food_prices)
-
-
-    
-
-
-
-    
-
-#     while True:
-#         pass
-
-
 # Necessary imports added ---
 import csv
 from selenium import webdriver
@@ -150,14 +27,16 @@ def get_food_data():
         driver.get('https://magicpin.in/New-Delhi/Paharganj/Restaurant/Eatfit/store/61a193/delivery/')
 
         food_types = ['Kulcha Burger', 'Chinese Fried Rice & Noodle Bowl', '3 Layer Rice Bowl', 'Indian Thalis', 'Breakfast And Snacks', 'Desserts & Juices']
-
+       
+        # Added explicit wait for the page to load completely
         wait = WebDriverWait(driver, timeout=10)
 
+        wait.until(EC.presence_of_element_located((By.XPATH, "//header[@class='subListingsHeader']//p"))).click()
+        
 
         for curfoodtype in food_types:
 
 
-        # Added explicit wait for the page to load completely
 
                 food_names = []
                 food_prices = []
@@ -200,5 +79,8 @@ if __name__ == "__main__":
     get_food_data()
     make_csv(total_food_names, total_food_prices)
     # Quit the WebDriver
-    driver.quit()
+    print('Done Scraping Data -- find the csv file in the same directory :)')
+    
+    while True:
+        pass
 
